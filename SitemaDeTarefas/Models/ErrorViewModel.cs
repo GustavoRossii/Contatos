96e5e7bdc3c1cf902ed29aa75ffa76a710373e2e
Gustavo Rossi <GuhRossi06@hotmail.com>
2024-10-11 11:28:09 -0300

    Adicionar arquivos de projeto.

---
+++ SitemaDeTarefas/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SitemaDeTarefas.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
