f0f7e570b5a0b33451c799ecf01026fb32395957
duducamargo <edu.farias.camargo@gmail.com>
2024-10-14 21:26:05 -0300

    [add] Usuario class has been added

---
+++ SitemaDeTarefas/Models/Usuario.cs
@@ -0,0 +1,11 @@
+namespace SitemaDeTarefas.Models;
+
+public class Usuario
+{
+   public int Id {get; set;}
+    public string? Nome {get; set; }
+    public string? Email {get; set; }
+    public string? Senha {get; set; }
+    public DateTime Criado_em {get; set; }
+
+}
